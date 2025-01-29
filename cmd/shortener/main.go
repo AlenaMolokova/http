5b0c4fef91fbe8f07140dd53fb8db6e974910212
alena.molokova@davinchi.edu.ar <alena.molokova@davinci.edu.ar>
2025-01-29 18:01:02 -0200

    v1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,32 @@
 package main
 
-func main() {}
+import (
+    "net/http"
+    "log"
+	"io"
+)
+
+func main() {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", handleURL)
+
+    log.Println("Starting server on :8080")
+    err := http.ListenAndServe(":8080", mux)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func handleURL(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        http.Error(w, "Only POST method is allowed", http.StatusBadRequest)
+        return
+    }
+
+    
+    if r.URL.Path != "/" {
+        http.Error(w, "Invalid path", http.StatusBadRequest)
+        return
+    }
+    
+    
