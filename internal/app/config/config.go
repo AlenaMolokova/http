3b004b1fccaf91872218ab705e5032d9b660160e
AlenaMolokova <alena.v.molokova@gmail.com>
2025-02-04 12:10:45 -0200

    iter5

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -2,20 +2,38 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 type Config struct {
-	ServerAddress string // флаг -a
-	BaseURL       string // флаг -b
+	ServerAddress string
+	BaseURL       string
 }
 
 func InitConfig() *Config {
 	cfg := &Config{}
 
-	flag.StringVar(&cfg.ServerAddress, "a", "localhost:8080", "Адрес запуска HTTP-сервера")
-	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "Базовый адрес результирующего сокращённого URL")
+	envServerAddress := os.Getenv("SERVER_ADDRESS")
+	envBaseURL := os.Getenv("BASE_URL")
 
+	defaultServerAddress := "localhost:8080"
+	defaultBaseURL := "http://localhost:8080"
+
+	flag.StringVar(&cfg.ServerAddress, "a", "", "Адрес запуска HTTP-сервера")
+	flag.StringVar(&cfg.BaseURL, "b", "", "Базовый адрес результирующего сокращённого URL")
 	flag.Parse()
 
+	cfg.ServerAddress = firstNonEmpty(envServerAddress, cfg.ServerAddress, defaultServerAddress)
+	cfg.BaseURL = firstNonEmpty(envBaseURL, cfg.BaseURL, defaultBaseURL)
+
 	return cfg
+}
+
+func firstNonEmpty(values ...string) string {
+	for _, v := range values {
+		if v != "" {
+			return v
+		}
+	}
+	return ""
 }
