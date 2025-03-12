66eff8af11cf00a803f2deb0e573784f794766f7
AlenaMolokova <alena.v.molokova@gmail.com>
2025-03-12 12:04:29 -0200

    v3

--- internal/app/storage/database/queries.go
+++ internal/app/storage/database/queries.go
@@ -1,18 +1,6 @@
 package database
 
 const (
-	createTableQuery = `
-		CREATE TABLE IF NOT EXISTS url_storage (
-			short_id TEXT PRIMARY KEY,
-			original_url TEXT NOT NULL,
-			created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-			last_accessed_at TIMESTAMP WITH TIME ZONE 
-		);
-
-		CREATE UNIQUE INDEX IF NOT EXISTS idx_original_url 
-		ON url_storage(original_url);
-	`
-
 	insertURLQuery = `
 		INSERT INTO url_storage (short_id, original_url) 
 		VALUES ($1, $2) 
