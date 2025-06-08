ea2190be42b64c407c1d28778d9af16070e8045e
AlenaMolokova <alena.v.molokova@gmail.com>
2025-06-08 18:21:35 -0200

    gofmt

--- cmd/shortener/gencert/main.go
+++ cmd/shortener/gencert/main.go
@@ -49,7 +49,6 @@ func generateCertificate() error {
 		return fmt.Errorf("ошибка создания сертификата: %w", err)
 	}
 
-	
 	// Сохраняем сертификат в файл
 	certOut, err := os.Create("server.crt")
 	if err != nil {
